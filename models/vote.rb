fb2d683ab0a735e778b226bf74554d15d636e0fd
Fabian Becker <halfdan@xnorfz.de>
2016-01-30 12:42:21 +0100

    Auto accept badge if votes > 2

--- models/vote.rb
+++ models/vote.rb
@@ -1,4 +1,10 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :badge_change
+
+  after_create do
+    if self.badge_change.votes.count > 2
+      badge_change.accept!
+    end
+  end
 end
